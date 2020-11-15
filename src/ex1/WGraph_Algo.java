b5622eb2033d47348db8fd47de399c2b2f89e5e1
shilo ben natan <shilo.b.n@gmail.com>
2020-11-15 21:38:24 +0200

    add graph example method and bug fixed at "spd"(set tag method added)

--- src/ex1/WGraph_Algo.java
+++ src/ex1/WGraph_Algo.java
@@ -1,5 +1,6 @@
 package ex1;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -24,11 +25,23 @@ public class WGraph_Algo  implements weighted_graph_algorithms{
 
     @Override
     public boolean isConnected() {
-        return false;
+        if (this.g1.getNods().size()==1||this.g1.getNods().size()==0)
+            return true;
+        Iterator<node_info> iterator=this.g1.getNods().iterator();
+        node_info n1=new NodeInfo();
+        n1=iterator.next();
+        shortestPathDist(n1.getKey(),iterator.next().getKey());
+        int temp=this.g1.getV().size();
+        return (this.g1.getV().size()==nodeCount);
     }
-
+    public void setTags()
+    {
+        for(node_info n1: g1.getV())
+            n1.setTag(-1);
+    }
     @Override
     public double shortestPathDist(int src, int dest) {
+        setTags();
         nodeCount=0;
         NodeInfo temp=new NodeInfo();
         if(!g1.getNods().contains(g1.getNode(src))||!g1.getNods().contains(g1.getNode(dest)))
--- src/tests/WGraph_AlgoTest.java
+++ src/tests/WGraph_AlgoTest.java
@@ -5,15 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class WGraph_AlgoTest {
-
-    @Test
-    public void distTest(){
+    public WGraph_Algo graphExample()
+    {
         WGraph_Algo wg=new WGraph_Algo();
-        if(wg.shortestPathDist(2,5)!=-1)
-            fail("u cant search number that not on the graph");
         wg.getGraph().addNode(3);
-        if(wg.shortestPathDist(3,3)!=0)
-            fail("its not return the right number in the case we send equals values");
         wg.getGraph().addNode(5);
         wg.getGraph().addNode(1);
         wg.getGraph().addNode(2);
@@ -31,7 +26,20 @@ public class WGraph_AlgoTest {
         wg.getGraph().connect(18,10,2);
         wg.getGraph().connect(10,9,2);
         wg.getGraph().connect(10,4,1);
+        return wg;
+    }
+    @Test
+    public void distTest(){
+        WGraph_Algo wg=new WGraph_Algo();
+        if(wg.shortestPathDist(2,5)!=-1)
+            fail("u cant search number that not on the graph");
+        wg.getGraph().addNode(3);
+        if(wg.shortestPathDist(3,3)!=0)
+            fail("its not return the right number in the case we send equals values");
+        wg=graphExample();
         if(wg.shortestPathDist(1,9)!=7)
             fail("shortestPathDist not work properly");
+        System.out.println(wg.isConnected());
+        System.out.println(wg.getGraph().getV().size());
     }
 }
